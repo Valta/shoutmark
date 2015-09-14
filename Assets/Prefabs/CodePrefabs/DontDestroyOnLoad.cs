adda66ee3cc83bf318ee83d8522bd8d33c746be6
Valta <vakoilusatelliitti@hotmail.com>
2015-09-14 14:55:53 +0300

    lisatty main menu ym

---
+++ Assets/Prefabs/CodePrefabs/DontDestroyOnLoad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+
+    private static bool thisObjectHasSpawned = false;
+
+    void Awake()
+    {
+        if (thisObjectHasSpawned == false)
+        {
+            thisObjectHasSpawned = true;
+            
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            // This deletes new objects of same type when loading
+            // new scenes but keeps the first one
+            DestroyImmediate(gameObject);
+        }
+    }
+
+}
---
+++ Assets/mainmenu_scene/MainMenuScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.EventSystems;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Canvas Canvas;
+	public RectTransform[] Panels;
+
+	// Use this for initialization
+	void Start () {
+		Canvas.gameObject.SetActive(true);
+		for (int i = 0; i < Panels.Length; i++)
+		{
+			Panels[i].gameObject.SetActive(false);
+		}
+		Panels[0].gameObject.SetActive(true);
+		//EventSystem.current.SetSelectedGameObject(GameObject.Find("StartButton"));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void SwitchToPanel(int panelNumber)
+	{
+		for (int i = 0; i < Panels.Length; i++)
+		{
+			Panels[i].gameObject.SetActive(false);
+		}
+		Panels[panelNumber].gameObject.SetActive(true);
+		
+	}
+	
+	public void Loadscene(int levelNumber)
+	{
+		if (levelNumber < EditorBuildSettings.scenes.Length)
+		{
+			//SwitchToPanel(3); //loading screen panel if we have one
+			Application.LoadLevel(levelNumber);
+		}
+		
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit(); // doesnt work in editor, only in build
+	}
+
+}
