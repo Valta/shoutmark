d28c8419c45e286f964a93fa1d1b98e5e38ee7ed
Valta <vakoilusatelliitti@hotmail.com>
2015-09-22 15:28:32 +0300

    pausemenu prefabbi

--- Assets/mainmenu_scene/PausemenuScript.cs
+++ Assets/mainmenu_scene/PausemenuScript.cs
@@ -3,6 +3,42 @@ using System.Collections;
 
 public class PausemenuScript : MonoBehaviour {
 
+
+	public static PausemenuScript instance;
+	
+	private static bool pauseManagerHasSpawned = false;
+
+	public Canvas PauseMenuCanvas;
+	public RectTransform PausemenuPanel;
+	public RectTransform PauseOptionsPanel;
+
+	void Awake()
+	{
+		if (pauseManagerHasSpawned == false)
+		{
+			pauseManagerHasSpawned = true;
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+			
+		}
+		else
+		{
+			// This deletes new objects of same type when loading
+			// new scenes but keeps the first one
+			DestroyImmediate(gameObject);
+		}
+		
+		if (PausemenuPanel)
+		{
+			PausemenuPanel.gameObject.SetActive(false);
+		}
+		if (PauseOptionsPanel)
+		{
+			PauseOptionsPanel.gameObject.SetActive(false);
+		}
+		
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +46,12 @@ public class PausemenuScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.M)) {
-			PauseGame();
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			if (Application.loadedLevel != 0 && !PauseOptionsPanel.gameObject.active)
+			{
+				PauseGame();
+			}
 		}
 	
 	}
@@ -21,12 +61,63 @@ public class PausemenuScript : MonoBehaviour {
 		if (!_TIMER.paused())
 		{
 			_TIMER.set_pause (true);
+			PausemenuPanel.gameObject.SetActive(true);
 			Debug.Log("game is paused");
 		}
 		else
 		{
 			_TIMER.set_pause(false);
+			PausemenuPanel.gameObject.SetActive(false);
 			Debug.Log("game is not paused");
 		}
+	}
+
+	public void GoToOptions()
+	{
+		if (PausemenuPanel)
+		{
+			PausemenuPanel.gameObject.SetActive(false);
+		}
+		if (PauseOptionsPanel)
+		{
+			PauseOptionsPanel.gameObject.SetActive(true);
+		}
+
+	}
+
+	public void GoBackFromOptions()
+	{
+		if (PauseOptionsPanel)
+		{
+			PauseOptionsPanel.gameObject.SetActive(false);
+		}
+		if (PausemenuPanel)
+		{
+			PausemenuPanel.gameObject.SetActive(true);
+		}
+		
+	}
+	
+	public void GoToMainMenu()
+	{
+		if (PauseOptionsPanel)
+		{
+			PauseOptionsPanel.gameObject.SetActive(false);
+		}
+		if (PausemenuPanel)
+		{
+			PausemenuPanel.gameObject.SetActive(false);
+		}
+
+		if (Application.loadedLevel != 0)
+		{
+			_TIMER.set_pause(false);
+			Application.LoadLevel(0);
+		}
+		else
+		{
+			Debug.Log("Your current scene is not in the build, add it");
+		}
+		
 	}
 }
