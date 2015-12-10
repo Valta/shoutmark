25682227470d524809322f184cc8cf7e6c1a70dd
Valta <vakoilusatelliitti@hotmail.com>
2015-12-10 16:36:23 +0200

    moving BG menussa

---
+++ Assets/TextureMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureMover : MonoBehaviour {
+
+    private float offSetX = 0.0f;
+    private float offSetY = 0.0f;
+
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+
+        renderer = GetComponent<Renderer>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        renderer.material.mainTextureOffset = new Vector2(offSetX, offSetY);
+        offSetX -= 0.1f * Time.deltaTime;
+        offSetY -= 0.05f * Time.deltaTime;
+	
+	}
+}
