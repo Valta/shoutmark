ea653234e32cd8cabbe41f040cfccf4e03316cb0
JarkkoMatilainen <jarkkom@windowslive.com>
2015-12-14 18:19:58 +0200

    uusi laaseriskripti

--- Assets/game_scene/enemy/laser_script.cs
+++ Assets/game_scene/enemy/laser_script.cs
@@ -1,103 +1,110 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class laser_script : MonoBehaviour {
-
-    private const int MAX_BEAMS = 20; // TODO: estimate of how many beams are needed at one time
-    public GameObject laserbeam; // Set prefab in editor
-    private float beam_length = 2.0f;
-    private float[] speed = new float[MAX_BEAMS + 1];
-    private _TILEMAP tilemap;
-
-    private Vector3[] beam_point = new Vector3[MAX_BEAMS + 1]; // front point of the beam, can be used for detecting hits
-    private Vector3[] beam_direction = new Vector3[MAX_BEAMS + 1]; // beam goes this way
-    private LineRenderer[] beams = new LineRenderer[MAX_BEAMS + 1];
-
-    private Vector3 storage = new Vector3(0.0f, 10.0f, 0.0f);
-    private Vector3 storage_dir = new Vector3(1.0f, 10.0f, 0.0f);
-
-    public void shoot_beam(Vector3 gunpoint, Vector3 shooter)
-    {
-        // get first free beam
-        int id = get_unused_beam_id();
-        if (id >= 0)
-        {
-            speed[id] = 2.0f;
-            beam_point[id] = shooter;
-            beam_direction[id] = Vector3.Normalize(gunpoint - shooter); // deviation?
-            Debug.Log("using beam " + id + " from point " + shooter);
-        }
-        else Debug.Log("Error: All lasers in use! More ammo needed!");
-    }
-    int get_unused_beam_id()
-    {
-        int id = -1;
-        for (int i = 0; i < MAX_BEAMS + 1; ++i)
-        {
-            if (speed[i] == 0)
-                return i;
-        }
-        return id;
-    }
-	// Use this for initialization
-	void Start () 
-    {
-        tilemap = GameObject.Find("_GLOBAL_SCRIPTS").GetComponent<_TILEMAP>();
-        for (int i = 0; i < MAX_BEAMS + 1; ++i)
-        {
-            GameObject beam = (GameObject)Instantiate(laserbeam, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
-            beam.transform.SetParent(transform);
-            beam.transform.localPosition = transform.position; //new Vector3(0.0f, 0.0f, 0.0f);
-            beams[i] = beam.GetComponent<LineRenderer>();
-            beam_point[i] = storage;
-            beam_direction[i] = storage_dir;
-            beams[i].SetPosition(0, storage);
-            beams[i].SetPosition(1, storage_dir);
-            speed[i] = 0.0f;
-        }
-	}
+public class laser_script : MonoBehaviour
+{
+	private const int MAX_NUMBER_OF_LASERS = 120;
+	private const float SPEED = 6.0f;
+	private const float DISTANCE = 0.5f;
 	
-	// Update is called once per frame
-	void Update () 
-    {
-        update_beam_positions();
-        //check_beam_collisions();
-        draw_beams();
+	public GameObject laser;
+	private Transform[] lasers = new Transform[MAX_NUMBER_OF_LASERS];
+	private bool[] laser_active = new bool[MAX_NUMBER_OF_LASERS];
+	private float[] laser_direction = new float[MAX_NUMBER_OF_LASERS];
+	private _TILEMAP tilemap;
+
+
+
+	void Start()
+	{
+		tilemap = GameObject.Find("_GLOBAL_SCRIPTS").GetComponent<_TILEMAP>();
+		initialize_lasers();
 	}
-    
-    void draw_beams()
-    {
-        for (int i = 0; i < MAX_BEAMS + 1; ++i)
-        {
-            beams[i].SetPosition(0, beam_point[i]);
-            beams[i].SetPosition(1, trail_direction(beam_direction[i]));
-        }
-    }
-    void update_beam_positions()
-    {
-        for (int i = 0; i < MAX_BEAMS + 1; ++i)
-        {
-            beam_point[i] += beam_direction[i] * _TIMER.deltatime() * speed[i];
-        }
-    }
-    void check_beam_collisions()
-    {
-        for (int i = 0; i < MAX_BEAMS + 1; ++i)
-        {
-            if (!tilemap.floor(beam_point[i].x, -beam_point[i].z))
-            {
-                beam_point[i] = storage;
-                beam_direction[i] = storage_dir;
-                speed[i] = 0;
-            }
-        }
-    }
-// TODO: right coordinates! dammit!
-    Vector3 trail_direction(Vector3 direction)
-    {
-        //Vector3 endpoint = new Vector3(-direction.x, 0, direction.y);
-        return Vector3.Normalize(direction) * beam_length;
-    }
+
+
+
+	public void initialize_lasers()
+	{
+		/*
+		bool destroying_lasers = true;
+		while (destroying_lasers)
+		{
+			destroying_lasers = false;
+			GameObject old_lasers = GameObject.Find("laser_parent(Clone)");
+			if (old_lasers)
+			{
+				Destroy(old_lasers);
+				//destroing_lasers = true;
+			}
+		}
+		*/
+		
+		for (int a = 0; a < MAX_NUMBER_OF_LASERS; a++)
+		{
+			GameObject new_laser = (GameObject)Instantiate(	laser,
+															new Vector3(1000.0f, 1000.0f, 1000.0f),
+															Quaternion.identity);
+			lasers[a] = new_laser.transform;
+			laser_active[a] = false;
+		}
+	}
+
+
+
+	public void shoot(Vector3 position, float direction)
+	{
+		for (int b = 0; b < 2; b++)
+		{
+			int index = -1;
+			for (int a = 0; a < MAX_NUMBER_OF_LASERS; a++)
+			{
+				if (!laser_active[a]) index = a;
+			}
+			if (index >= 0)
+			{
+				laser_active[index] = true;
+				laser_direction[index] = direction;
+				lasers[index].position =
+					new Vector3(position.x + (b - 0.5f) * DISTANCE * Mathf.Cos((direction + 90.0f) * 3.14159f / 180.0f),
+								0.0f,
+								position.z - (b - 0.5f) * DISTANCE * Mathf.Sin((direction + 90.0f) * 3.14159f / 180.0f));
+				lasers[index].rotation = Quaternion.Euler(0.0f, laser_direction[index], 0.0f);
+			}
+			else
+			{
+				Debug.Log("OUT OF LASERS!");
+			}
+		}
+	}
+
+
+
+	private void destroy_laser(int index)
+	{
+		laser_active[index] = false;
+		lasers[index].position = new Vector3(1000.0f, 1000.0f, 1000.0f);
+	}
+
+
+
+	void Update()
+	{
+		float dt = _TIMER.deltatime();
+		for (int a = 0; a < MAX_NUMBER_OF_LASERS; a++)
+		{
+			if (laser_active[a])
+			{
+				lasers[a].position += new Vector3(	SPEED * dt * Mathf.Cos(laser_direction[a] * 3.14159f / 180.0f),
+													0.0f,
+													-SPEED * dt * Mathf.Sin(laser_direction[a] * 3.14159f / 180.0f));
+				if (!tilemap.can_a_tile_be_pushed_here((int)lasers[a].position.x, (int)(-lasers[a].position.z)))
+				{
+					destroy_laser(a);
+				}
+			}
+		}
+	}
+
+
+
 }
-
-
---
+++ Assets/game_scene/laser/laser_test_POISTA.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laser_test_POISTA : MonoBehaviour
+{
+
+
+	void Start()
+	{
+	
+	}
+
+
+	void Update()
+	{
+		transform.RotateAround(transform.position, new Vector3(0.0f, 1.0f, 0.0f), _TIMER.deltatime() * 20.0f);
+		if (Input.GetKeyDown(KeyCode.Y))
+		{
+			laser_script laser = gameObject.GetComponent<laser_script>();
+			laser.shoot(transform.position, transform.rotation.eulerAngles.y);
+		}
+	}
+	
+	
+}
